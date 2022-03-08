2eff68d18f88f1c6aad391904577f17d51e2c11f
istiklal gunes <60663260+istiklalg@users.noreply.github.com>
2022-03-08 21:03:32 +0300

    Add files via upload

---
+++ api/routes/atibaClusterRoutes.js
@@ -0,0 +1,28 @@
+/**
+ * @author istiklal
+ */
+
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const clusterController = require("../controllers/atibaClusterController");
+
+const Logger = require('../logger');
+const logger = new Logger("cluster_router");
+
+
+//router.get("/", clusterController.getHome);
+//router.get("/health_check", clusterController.healthOfApi);
+router.get("/status", clusterController.getClusterStatus);
+// router.get("/add_node", clusterController.addNodeToCluster);
+router.get("/change_struct", clusterController.changeNodeStructForCluster);
+router.get("/remove_node", clusterController.removeNodeFromCluster);
+
+router.post("/add_node", clusterController.addNodeToCluster);
+router.post("/change_struct", clusterController.changeNodeStructForCluster);
+router.post("/remove_node", clusterController.removeNodeFromCluster);
+
+// router.get("/", (req, res, next) => {logger.debug("geldiiii")});
+
+
+exports.router = router;
